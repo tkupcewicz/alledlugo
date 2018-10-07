f6593aa0f761dbb31fd2924c9bcebb20e523754f
tkupcewicz <t.kupcewicz@gmail.com>
2018-10-07 17:37:45 +0200

    Finished version 1.0

--- content.js
+++ content.js
@@ -1,44 +1,34 @@
-const priceClasses = {
-  'listings': 'ecb7eff',
-  'mainPrice': 'm-price',
-  'horizontalList': '_07bcb_2-O4V _07bcb_2A6zP',
-  'delivery': 'original-price _1pa4o',
-  'discount': '_07bcb_2-O4V _07bcb_3AoIh ',
-  'itemsKit': '_4795a692 _520a029f',
-  'kitSumUp': 'defac815 _98dc940e f396bdb3',
-  'sumUpBefore': '_98dc940e _74d596c8',
-  'deliveryFrom': '_2SMFlpDz'
-}
+function setCaretPosition(elem, caretPos) {
 
-var hourRate = 8.5;
-
-function convertPrice(textPrice) {
-
-  var strippedPrice = textPrice.replace(/[^\d,]/g, '').replace(/,/g, '.');
-  var priceFloat = parseFloat(strippedPrice);
-  var priceInHours = priceFloat / hourRate;
-  var days = Math.floor(priceInHours/24);
-  var hours = priceInHours - days * 24;
-  var minutes = Math.floor((hours - Math.floor(hours)) * 60);
-  hours = Math.floor(hours);
-  var finalTime = days + "d " + hours + "h " + minutes + "m";
-  return finalTime;
-}
-
-function convertCollection(collection) {
-
-  var priceToConvert;
-
-  for (var i = 0, l = collection.length; i < l; i++) {
-    priceToConvert = collection[i].innerText;
-    if (priceToConvert) {
-      console.log(convertPrice(priceToConvert));
-      collection[i].innerHTML = convertPrice(priceToConvert);
+  if(elem.createTextRange) {
+      var range = elem.createTextRange();
+      range.move('character', caretPos);
+      range.select();
+  }
+  else {
+    if(elem.selectionStart) {
+      elem.focus();
+      elem.setSelectionRange(caretPos, caretPos);
     }
+    else
+      elem.focus();
   }
 }
 
-for (var key in priceClasses) {
-  prices = document.getElementsByClassName(priceClasses[key]);
-  convertCollection(prices);
+function fillUpMail() {
+  var email = '';
+  chrome.storage.sync.get({
+    ngAddress: 'default',
+  }, function(items) {
+    email = items.ngAddress;
+    email = [email.slice(0, email.indexOf("@")), "+", email.slice(email.indexOf("@"))].join('');
+    document.activeElement.value = email;
+    setCaretPosition(document.activeElement, email.indexOf("@"));
+  });
 }
+ 
+chrome.runtime.onMessage.addListener(function (msg, _, sendResponse) {
+  if(msg == "modify_input") {
+    fillUpMail();
+  }
+});
